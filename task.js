78350dd2f12e5812b166fd1ee0a80b1a7dc708f9
YrChek <92719549+YrChek@users.noreply.github.com>
2022-09-17 19:52:22 +0500

    Изменение формулы расчета

--- task.js
+++ task.js
@@ -1,7 +1,8 @@
+// Все равно не совсем понял условие задания.
+
 const img = document.getElementById('cookie');
 const count = document.getElementById('clicker__counter');
 const speed = document.getElementById('clicker__speed');
-let factor = 0;
 let timeStart = 0;
 let timeStop;
 let num = 1;
@@ -9,11 +10,7 @@ img.onclick = () => {
     img.width += 50 * num;
     count.textContent = Number(count.textContent) + 1;
     num *= -1;
-    if (factor === 0) {
-        timeStart = Date.now();
-        factor = 1;
-    } else {
-        timeStop = Date.now()
-        speed.textContent = Math.round(Number(count.textContent) / ((timeStop - timeStart) / 1000) * 100) / 100;
-    }
+    timeStop = Date.now();
+    speed.textContent = Math.round(1 / ((timeStop - timeStart) / 1000) * 100) / 100;
+    timeStart = Date.now();
 }
